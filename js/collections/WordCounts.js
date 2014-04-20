1bd24011f41f1400b40c2e4b200e3ca5a66c0653
some things are not up to us <utres.inflati.ambulamus@gmail.com>
2014-04-19 19:26:32 -0700

    rename word count model and collection

--- js/collections/WordCounts.js
+++ js/collections/WordCounts.js
@@ -3,10 +3,10 @@ var LocalStorage = require('backbone.localstorage');
 
 var log = require('bows')('WordWarModel');
 
-var WordWar = require('../models/WordWar');
+var WordCount = require('../models/WordCount');
 
 module.exports = Backbone.Collection.extend({
-  model: WordWar,
+  model: WordCount,
 
   localStorage: new LocalStorage('wordWars')
 });
