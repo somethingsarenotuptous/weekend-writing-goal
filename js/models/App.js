100877e1800582af1df645220603dc803f4649ee
some things are not up to us <utres.inflati.ambulamus@gmail.com>
2014-04-19 19:28:04 -0700

    remove WordWars collection from app model; collections should be on views

--- js/models/App.js
+++ js/models/App.js
@@ -1,11 +1,8 @@
 var Backbone = require('backbone');
 
-var WordWars = require('../collections/WordWars');
 var log = require('bows')('AppModel');
 
 module.exports = Backbone.Model.extend({
-  collection: new WordWars(),
-
   initialize: function() {
     log('Initialized AppModel.');
     this.countdown = this.get('countdown');
